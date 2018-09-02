281e1f7b3908b93b5689d3e424a14b5413d9b926
Nicholas Dunn <nexela@gmail.com>
2018-09-02 02:26:27 -0400

    Add the buffer corpse to library

--- data.lua
+++ data.lua
@@ -1,2 +1,28 @@
+local Data = require('__stdlib__/stdlib/data/data')
+
 require('prototypes/adjustment-pad')
 require('prototypes/styles')
+
+Data {
+    type = 'flying-text',
+    name = 'picker-flying-text',
+    flags = {'placeable-off-grid', 'not-on-map'},
+    time_to_live = 150,
+    speed = 0.05
+}
+
+Data {
+    name = 'picker-buffer-corpse-instant',
+    type = 'character-corpse',
+    picture = Data.Sprites.empty_pictures(),
+    selection_priority = 0,
+    time_to_live = 1
+}
+
+Data {
+    name = 'picker-buffer-corpse-inf',
+    type = 'character-corpse',
+    picture = Data.Sprites.empty_pictures(),
+    selection_priority = 0,
+    time_to_live = 2147483647
+}
