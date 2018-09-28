57221af7d62a8ddfaaaea2a718d64be571096720
Nicholas Dunn <nexela@gmail.com>
2018-09-27 21:44:21 -0400

    Add reload command

--- control.lua
+++ control.lua
@@ -3,9 +3,11 @@ require('utils/adjustment-pad').register_events()
 
 if require('__PickerAtheneum__/config').DEBUG then
     log(script.mod_name .. ' Developer Debug mode enabled')
+    commands.add_command('Picker.reload', 'Reset', function() game.reload_mods() end)
     require('__stdlib__/stdlib/core').create_stdlib_globals()
     require('__stdlib__/stdlib/scripts/quickstart')
 end
 
 commands.add_command('Picker.write-all', {'picker.write-all'}, interface.dump_all)
+
 remote.add_interface(script.mod_name, interface)
