7c488e61900069e8dddf14f8945d05a9526e4bfb
Nicholas Dunn <nexela@gmail.com>
2018-10-08 20:22:25 -0400

    Add quick Reload Mods hotkey when in debug mode

--- control.lua
+++ control.lua
@@ -2,10 +2,18 @@ local interface = require('__stdlib__/stdlib/scripts/interface')
 require('utils/adjustment-pad').register_events()
 
 if require('__PickerAtheneum__/config').DEBUG then
+    local Event = require('__stdlib__/stdlib/event/event')
+    local function reload(event)
+        local player = game.players[event.player_index]
+        player.clear_console()
+        game.print('Reloading Mods')
+        game.reload_mods()
+    end
     log(script.mod_name .. ' Developer Debug mode enabled')
-    commands.add_command('Picker.reload', 'Reset', function() game.reload_mods() end)
+    commands.add_command('Picker.reload', 'Reset', reload)
     require('__stdlib__/stdlib/core').create_stdlib_globals()
     require('__stdlib__/stdlib/scripts/quickstart')
+    Event.register('picker-reload-mods', reload)
 end
 
 commands.add_command('Picker.write-all', {'picker.write-all'}, interface.dump_all)
--- data-final-fixes.lua
+++ data-final-fixes.lua
@@ -1,4 +1,12 @@
 if require('__PickerAtheneum__/config').DEBUG then
     log('Making developer debug entities')
     require('__stdlib__/stdlib/data/developer/developer').make_test_entities()
+    data:extend{
+        {
+            type = 'custom-input',
+            name = 'picker-reload-mods',
+            key_sequence = 'CONTROL + SHIFT + R',
+            localised_name = 'Reload Mods'
+        }
+    }
 end
