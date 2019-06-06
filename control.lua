ae75d81874b1d4bcb371fa9ea88ba6625deff578
Nicholas Dunn <nexela@gmail.com>
2019-06-05 22:17:59 -0400

    Add Picker.write_ commands

--- control.lua
+++ control.lua
@@ -1,7 +1,9 @@
+local Game = require('__stdlib__/stdlib/game')
 local interface = require('__stdlib__/stdlib/scripts/interface')
 require('utils/adjustment-pad').register_events()
 
 if require('__PickerAtheneum__/config').DEBUG then
+    --require('__stdlib__/stdlib/scripts/console')
     local Event = require('__stdlib__/stdlib/event/event')
     local function reload(event)
         local player = game.players[event.player_index]
@@ -18,5 +20,8 @@ if require('__PickerAtheneum__/config').DEBUG then
 end
 
 commands.add_command('Picker.write-all', {'picker.write-all'}, interface.dump_all)
+commands.add_command('Picker.mods-to-file', "Write mods to file", Game.write_mods)
+commands.add_command('Picker.surface-to-file', "Write surface info to file", Game.write_surfaces)
+commands.add_command('Picker.statistics', "Write statistics info to file", Game.write_statistics)
 
 remote.add_interface(script.mod_name, interface)
