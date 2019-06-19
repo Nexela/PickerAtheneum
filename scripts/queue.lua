3725f8bbda57c7cd35d0134386a4126017d66ef3
TheStaplergun <staplergun@gmail.com>
2019-06-18 21:08:56 -0400

    Queue heavy mode safe

--- scripts/queue.lua
+++ scripts/queue.lua
@@ -19,34 +19,9 @@ script.on_event(token, function_to_call_each_turn)
 To remove your mod from the queue and de-register the listener, add this code somewhere in your mod once the work is done:
 --STDLIB-----------------------------------------------------------------------
 remote.call("PickerAtheneum","queue_remove",{token = global.queue_token})
-Event.remove(global.queue_token,function_to_call_each_turn)
-global.queue_token = nil
 --NO STD LIB-------------------------------------------------------------------
 remote.call("PickerAtheneum","queue_remove",{token = global.queue_token})
-script.on_event(global.queue_token,nil)
-global.queue_token = nil
 
-To handle registration properly on_load use:
---STDLIB-----------------------------------------------------------------------
-Event.on_load(
-    function()
-        if global.queue_token then
-                local token = remote.call("PickerAtheneum","queue_reestablish",{mod_name = "YourModName_QueuedTaskAbbreviation"}) --The additional queued task abbreviation allows you to queue multiple things within the same mod
-                global.queue_token = token
-                Event.register(token, function_to_call_each_turn)
-        end
-    end
-)
---NO STD LIB-------------------------------------------------------------------
-script.on_load(
-    function()
-        if global.queue_token then
-                local token = remote.call("PickerAtheneum","queue_reestablish",{mod_name = "YourModName_QueuedTaskAbbreviation"}) --The additional queued task abbreviation allows you to queue multiple things within the same mod
-                global.queue_token = token
-                script.on_event(token, function_to_call_each_turn)
-        end
-    end
-)
 ]]
 
 local Event = require('__stdlib__/stdlib/event/event')
@@ -54,16 +29,17 @@ local Interface = require('__stdlib__/stdlib/scripts/interface')
 
 local function queue_tick()
     local i = global.current_index
+    local q = global.queue
+    remote.call(q[i].name,q[i].f_name)
     i = i + 1
     if i > #global.queue then
         i = 1
     end
-    Event.raise_event(global.queue[i].token, {stuff = true})
+    global.current_index = i
 end
 
 local function queue_initialize()
     global.queue = {}
-    global.tokens_leased = {}
     global.running = true
     global.current_index = 1
     Event.register(defines.events.on_tick, queue_tick)
@@ -74,50 +50,33 @@ Interface['queue_add'] = function(data)
     if not global.running then
         queue_initialize()
     end
-    local new_token = Event.generate_event_name(data.mod_name .. '_queue_event')
-    --queue_generate_token()
     local q = global.queue
     q[#q + 1] = {
-        mod_name = data.mod_name,
-        token = new_token
+        name = data.name,
+        f_name = data.f_name
     }
-    global.tokens_leased[data.mod_name] = new_token
-    return new_token
+    return true
 end
 
 Interface['queue_remove'] = function(data)
-    local r_token = data.token
-    for index,token in pairs(global.queue) do
-        if token == r_token then
-            if index > global.current_index then
+    for i,q_pos in pairs(global.queue) do
+        if data.name == q_pos.name then
+            if (i > global.current_index) or (i > #global.queue) then
                 global.current_index = global.current_index - 1
             end
-            table.remove(global.queue, index)
+            table.remove(global.queue, i)
         end
     end
-    global.tokens_leased[r_token] = nil
-    if global.queue and not next(global.queue) then
+    if not next(global.queue) then
         global.queue = {}
         global.running = false
         Event.remove(defines.events.on_tick, queue_tick)
     end
 end
 
-
-Interface['queue_reestablish'] = function(data)
-    return global.tokens_leased[data.mod_name]
-end
-
-
-Event.on_load(
-    function()
-        global.running = global.running or false
-        if global.running then
-            queue_initialize()
-            for _,queue_data in pairs(global.queue) do
-                queue_data.token = Event.generate_event_name(queue_data.mod_name .. '_queue_event')
-                global.tokens_leased[queue_data.mod_name] = queue_data.token
-            end
-        end
+local function on_load()
+    if global.running then
+        Event.register(defines.events.on_tick,queue_tick)
     end
-)
+end
+Event.on_load(on_load)
