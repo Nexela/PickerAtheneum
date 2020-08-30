4c2ea9cac2615ab11d86807b019b88cf05012ee1
Nexela <nexela@gmail.com>
2020-08-29 20:14:33 -0400

    Updates

--- config-quickstart.lua
+++ config-quickstart.lua
@@ -1,14 +1,42 @@
 -- Settings used for the quickstart script,
 -- if an item doesn't exist it is ignored.
-return {
-    cheat_mode = true,
-    always_day = true,
-    power_armor = 'power-armor-mk2',
+local items, equipment
+if script.active_mods['EditorExtensions'] then
+    items = {
+        ['ee-infinity-chest'] = 50,
+        ['ee-infinity-pipe'] = 50,
+        ['ee-infinity-heat-interface'] = 50,
+        ['ee-infinity-accumulator'] = 50,
+        ['construction-robot'] = 50,
+        ['pipe'] = 50,
+        ['fast-inserter'] = 50
+    }
+    equipment = {
+        'ee-infinity-fusion-reactor-equipment',
+        'ee-super-personal-roboport-equipment',
+        'belt-immunity-equipment'
+    }
+else
+    items = {
+        ['infinity-chest'] = 50,
+        ['infinity-pipe'] = 50,
+        ['heat-interface'] = 50,
+        ['construction-robot'] = 50,
+        ['pipe'] = 50,
+        ['fast-inserter'] = 50
+    }
     equipment = {
         'fusion-reactor-equipment',
         'personal-roboport-mk2-equipment',
         'belt-immunity-equipment'
-    },
+    }
+end
+
+return {
+    cheat_mode = true,
+    always_day = true,
+    power_armor = 'power-armor-mk2',
+    equipment = equipment,
     destroy_everything = true,
     disable_rso_starting = true,
     disable_rso_chunk = true,
@@ -22,16 +50,5 @@ return {
     chunk_bounds = true,
     center_map_tag = true,
     setup_power = true,
-    items = {
-        ['infinity-chest'] = 50,
-        ['infinity-pipe'] = 50,
-        ['heat-interface'] = 50,
-        ['infinity-accumulator'] = 50,
-        ['construction-robot'] = 50,
-        ['assembling-machine-3'] = 50,
-        ['pipe'] = 50,
-        ['fast-inserter'] = 50,
-        -- ['picker-note'] = 50,
-        -- ['picker-sign'] = 50
-    }
+    items = items
 }
