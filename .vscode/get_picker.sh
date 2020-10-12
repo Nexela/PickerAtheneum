828bfb5cfe55c3de478a708b8a63248bfdba49e5
Nexela <nexela@gmail.com>
2020-10-11 22:42:03 -0400

    More get_picker fixes

--- .vscode/get_picker.sh
+++ .vscode/get_picker.sh
@@ -15,6 +15,6 @@ git clone git@github.com:nexela/PickerFill.git
 git clone git@github.com:nexela/Nanobots.git
 git clone --recurse-submodules git@github.com:Afforess/Factorio-Stdlib.git stdlib
 git clone git@github.com:factoriolib/flib.git
-ln -s PickerAtheneum/.vscode/linux .vscode
-ln -s $(basename $(pwd))/stdlib/luacheckrc/.luacheckrc ../.luacheckrc
-ln -s $(basename $(pwd))/stdlib/luacheckrc/.lua-format ../.lua-format
+ln -s $(pwd)/PickerAtheneum/.vscode/linux/ .vscode
+ln -s $(pwd)/stdlib/luacheckrc/.luacheckrc ../.luacheckrc
+ln -s $(pwd)/stdlib/luacheckrc/.lua-format ../.lua-format
