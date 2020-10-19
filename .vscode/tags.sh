c9ca90d89f3b683767d3a6af51502a473a250630
Nexela <nexela@gmail.com>
2020-10-18 23:40:01 -0400

    Update tags script

--- .vscode/tags.sh
+++ .vscode/tags.sh
@@ -1,8 +1,19 @@
+#!/bin/bash
+clear
+echo "Starting Script"
 git tag -l | while read t; do
-    n=$(echo $t | sed "s/vv/v/g")
+    n=$(echo $t | sed "s/^\([0-9]\)/v\1/g")
+    # echo $t -- $n
     if [[ "$n" != "$t" ]]; then
+        echo "Does not Match $t -- $n"
         git tag $n $t
         git tag -d $t
     fi
 done
-## update tags ref
+echo "Do you wish to update refs/tags/*"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes) git push --tags --prune origin refs/tags/*; break;;
+        No) exit;;
+    esac
+done
